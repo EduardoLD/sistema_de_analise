7298e91c183259e4a796b72591ba649022de8a33
eduardold <eduardold@whbbrasil.com.br>
2024-03-11 16:35:27 -0300

    criado versão inicial da tela preview de gestão

--- app/Controller/GestaoController.php
+++ app/Controller/GestaoController.php
@@ -6,16 +6,17 @@ Class GestaoController extends CadastroController
     public function index()
     {
         $p = filter_input(INPUT_GET, 'p', FILTER_SANITIZE_SPECIAL_CHARS);
-        
+        $gestao = $this->busca_gestao();
+        // ver($gestao);
         carrega_twig('Preview_gestao.twig', [
-            'p'             => $p,
-            'existe_gestao' => false//$this->existeGestao()
+            'p'      => $p,
+            'gestao' => $gestao
         ]);
     }
 
-    public function existeGestao()
+    public function busca_gestao()
     {
-        return $this->model->execQuery("SELECT * FROM gestao");
+        return $this->model->execQuery("SELECT * FROM gestao ORDER BY id LIMIT 1", false, false);
     }
 
 }
