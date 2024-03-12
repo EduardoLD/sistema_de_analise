6401915a1eff4e1a69859bfec4beece942e9bf71
eduardold <eduardold@whbbrasil.com.br>
2024-03-12 11:40:16 -0300

    melhorado preview gestao

--- app/Controller/GestaoController.php
+++ app/Controller/GestaoController.php
@@ -5,9 +5,13 @@ Class GestaoController extends CadastroController
 
     public function index()
     {
-        $p = filter_input(INPUT_GET, 'p', FILTER_SANITIZE_SPECIAL_CHARS);
+        $p      = filter_input(INPUT_GET, 'p', FILTER_SANITIZE_SPECIAL_CHARS);
         $gestao = $this->busca_gestao();
-        // ver($gestao);
+
+        if ( $gestao ) {
+            $this->adiciona_info_gestao($gestao);
+        }
+        
         carrega_twig('Preview_gestao.twig', [
             'p'      => $p,
             'gestao' => $gestao
@@ -19,4 +23,11 @@ Class GestaoController extends CadastroController
         return $this->model->execQuery("SELECT * FROM gestao ORDER BY id LIMIT 1", false, false);
     }
 
+    public function adiciona_info_gestao(&$gestao)
+    {
+        $gestao += [
+            'flo_stoploss_val'    => (($gestao['flo_stoploss_porc'] / 100) * $gestao['flo_banca_final']),
+            'flo_meta_diaria_val' => (($gestao['flo_meta_diaria_porc'] / 100) * $gestao['flo_banca_final'])
+        ];
+    }
 }
