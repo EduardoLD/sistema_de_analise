c474787e3f56f45f80641a90f9c1031cfa0c38ab
eduardold <eduardold@whbbrasil.com.br>
2024-03-13 13:48:03 -0300

    atualização 13:47 13/03

--- app/Controller/ApostasController.php
+++ app/Controller/ApostasController.php
@@ -15,8 +15,9 @@ Class ApostasController
         $p = filter_input(INPUT_GET, 'p', FILTER_SANITIZE_SPECIAL_CHARS);
 
         carrega_twig('Inclusao_apostas.twig', [
-            'p'     => $p,
-            'times' => $this->model->execQuery("SELECT * FROM times ORDER BY str_time ASC")
+            'p'           => $p,
+            'campeonatos' => $this->model->execQuery("SELECT * FROM campeonatos ORDER BY str_campeonato ASC"),
+            'times'       => $this->model->execQuery("SELECT * FROM times ORDER BY str_time ASC")
         ]);
     }
 
